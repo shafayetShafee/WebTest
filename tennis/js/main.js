c8d8d760c25fde4d6ac907d3a25aa0eda82b0ac8
shafayetShafee <seaborn.sicaro@gmail.com>
2020-07-11 20:32:12 +0600

    adding files for tennis website

---
+++ tennis/js/main.js
@@ -0,0 +1,8 @@
+
+function mail() {
+		document.getElementById("mail").style.visibility = "visible";
+	};
+
+function mailOff() {
+		document.getElementById("mail").style.visibility = "hidden";
+	};
