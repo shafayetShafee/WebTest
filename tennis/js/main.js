f652e92d931747fb152b3ff3628b1dd1e593d194
shafayetShafee <seaborn.sicaro@gmail.com>
2020-08-01 23:25:13 +0600

    adding scrolling effect, fixing the navbar hiding after event blur and fixing button outline color usingcss

--- tennis/js/main.js
+++ tennis/js/main.js
@@ -1,3 +1,29 @@
+// Dont know much about jQuery ... copied it from internet
+
+
+$(function (global) {
+
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    var target = $(event.target);
+    if (screenWidth < 992) {
+      if( target.not("collapsableNav")) {
+      	$("collapsableNav").collapse("hide");
+      }
+    }
+  });
+
+
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 800,
+	offset: 120
+});
 
 function mail() {
 		document.getElementById("mail").style.visibility = "visible";
